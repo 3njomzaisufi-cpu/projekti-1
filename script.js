d0733bb76600eb2e2e1bddc8658541d3fcf4d853
3njomzaisufi-cpu <3njomzaisufi@gmail.com>
2025-12-16 18:12:07 +0100

    Add JS selectors for navbar links and menu toggle buttons

--- script.js
+++ script.js
@@ -1,1 +1,3 @@
-
+const navLinks = document.querySelectorAll(".nav-menu .nav-link");
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
